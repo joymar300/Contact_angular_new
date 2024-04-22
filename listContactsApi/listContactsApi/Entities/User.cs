5b951a5238f25b58e001c34163e96ed967a1bb62
joymar300 <74611985+joymar300@users.noreply.github.com>
2024-04-22 12:52:34 -0500

    update
    create the API in .net 3
    -added jwt for authentication
    -create the tables, contacts, contacttype, user
    -I made the drivers.
    
    -I started a project in angular.
    -create navbar, table-contact, action-modal, form-contact, form-login components
    -create services for http requests with api

---
+++ listContactsApi/listContactsApi/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace listContactsApi.Entities
+{
+    public class User
+    {
+        public int userid { get; set; }
+        public string email { get; set; }
+
+        public string password { get; set; }
+    }
+}
